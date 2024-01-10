4a66d7851e835ad46c83783f1f89f440b1511236
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-01-10 12:33:52 +0900

    initial commit

---
+++ home.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+st.set_page_config(
+     initial_sidebar_state="collapsed" 
+)
+
+# Create an empty container
+name = ""
+password = ""
+placeholder = st.empty()
+
+# Insert a form in the container
+with placeholder.form("login"):
+    st.markdown("#### Enter your Information")
+    name = st.text_input("Name")
+    password = st.text_input("Password", type="password")
+    submit = st.form_submit_button("Start")
+
+if submit and name != "" and password != "":
+    # If the form is submitted and the email and password are correct,
+    # clear the form/container and display a success message
+    placeholder.empty()
+    print(name, password)
+    switch_page("chat")
+elif submit and name != "" or password != "":
+    st.error("Login failed")
+# else:
+#     pass
